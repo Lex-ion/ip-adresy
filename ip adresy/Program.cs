4195d3789cd266a0b1914996acc917bf20de14d3
Lex-ion <killkiisdog@gmail.com>
2022-11-30 17:08:56 +0100

    Přidejte soubory projektu.

---
+++ ip adresy/Program.cs
@@ -0,0 +1,16 @@
+﻿int maska;
+int cislo;
+Random rd = new Random();
+zacatek:
+for (int i = 0; i <3; i++) 
+{
+    cislo = rd.Next(0,255);
+    Console.Write(cislo+".");
+}
+cislo = rd.Next(0, 255);
+Console.Write(cislo + "/");
+maska = rd.Next(8,30);
+Console.Write(maska);
+Console.ReadKey();
+Console.Clear();
+goto zacatek;
